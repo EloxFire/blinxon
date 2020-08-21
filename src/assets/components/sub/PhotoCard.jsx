5d6a4dccd02984620e034d85a70f2ace68222c67
EloxFire <enzo.avagliano@ynov.com>
2020-08-21 12:26:44 +0200

    Commit update

--- src/assets/components/sub/PhotoCard.jsx
+++ src/assets/components/sub/PhotoCard.jsx
@@ -73,20 +73,22 @@ function PhotoCard(props){
   }
 
   return(
-    <div style={photoCardStyle().photoCard} className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-5 m-3 d-flex flex-column flex-xs-column flex-sm-column flex-md-column flex-lg-row p-0">
-      <div style={photoCardStyle().imageCard}></div>
-      <div style={photoCardStyle().textCard}>
-        <div className="headerCard col-12">
-          <h1 style={photoCardStyle().titleCard} className="m-0 p-0">{props.title}</h1>
-          <p style={photoCardStyle().subtitleCard}><span className="m-0 p-0">{props.subtitle}</span></p>
-          <hr className="m-1" style={{width:"60%"}}/>
+    <div style={photoCardStyle().photoCard} className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-5 m-3 d-flex flex-column flex-xs-column flex-sm-column flex-md-column flex-lg-column p-0">
+      <div className="d-flex flex-column flex-xs-column flex-sm-column flex-md-column flex-lg-row">
+        <div style={photoCardStyle().imageCard}></div>
+        <div style={photoCardStyle().textCard}>
+          <div className="headerCard col-12">
+            <h1 style={photoCardStyle().titleCard} className="m-0 p-0">{props.title}</h1>
+            <p style={photoCardStyle().subtitleCard}><span className="m-0 p-0">{props.subtitle}</span></p>
+            <hr className="m-1" style={{width:"60%"}}/>
+          </div>
+          <div style={{maxHeight:"50%", overflow:"auto"}}>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum aperiam similique nulla, repellat cum sequi nihil id officiis quas nobis eos voluptates. Non expedita commodi modi, vel vero eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum aperiam similique nulla, repellat cum sequi nihil id officiis quas nobis eos voluptates. Non expedita commodi modi, vel vero eius.</p>
+          </div>
+          <hr className="mt-3" style={{width:"60%"}}/>
         </div>
-        <div style={{maxHeight:"50%", overflow:"auto"}}>
-          <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum aperiam similique nulla, repellat cum sequi nihil id officiis quas nobis eos voluptates. Non expedita commodi modi, vel vero eius.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum aperiam similique nulla, repellat cum sequi nihil id officiis quas nobis eos voluptates. Non expedita commodi modi, vel vero eius.</p>
-        </div>
-        <hr className="mt-3" style={{width:"60%"}}/>
       </div>
-      <div className="footerCard d-flex flex-column justify-content-center align-items-center">
+      <div className="footerCard d-flex flex-wrap justify-content-center align-items-center">
         <h6>Quelques informations :</h6>
         <div className="d-flex flex-wrap justify-content-center">
           {props.age == null ? null : <li className="m-1"><span>Âge : </span>{props.age} ans</li>}
